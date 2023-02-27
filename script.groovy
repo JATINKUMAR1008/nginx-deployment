a62d967af550f6b94248e06bb4b5a4e563fe1e0f
JATINKUMAR1008 <97216852+JATINKUMAR1008@users.noreply.github.com>
2023-02-27 22:14:49 +0530

    second commit

--- script.groovy
+++ script.groovy
@@ -1,12 +1,12 @@
 def Build(){
     echo "Building the application..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+    withCredentials([usernamePassword(credentialsId: 'Docker', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
         sh "docker build -t mywebsite:1.0 ."
-        sh "docker tag mywebsite:1.0 ankitraz/mywebsite:1.0"
+        sh "docker tag mywebsite:1.0 jatin1008/mywebsite:1.0"
         /* if you want to push to dockerhub you need to build image with your repo name and tag.
         otherwise you"ll have to tag image manually like i did above.*/
-        sh "docker push ankitraz/mywebsite:1.0"
+        sh "docker push jatin1008/mywebsite:1.0"
     }
 }
 
@@ -16,10 +16,10 @@ def Test(){
 
 def Deploy(){
     echo "Deploying the application..."
-    def dockerCmd = "docker run -d -p 80:80 ankitraz/mywebsite:1.0"
-    sshagent(['github-ssh-key']) {
-        sh "ssh -o StrictHostKeyChecking=no root@64.227.108.131 ${dockerCmd}"
-    }
+    def dockerCmd = "docker run -d -p 80:80 jatin1008/mywebsite:1.0"
+    
+        sh "${dockerCmd}"
+    
 }
 
 return this
